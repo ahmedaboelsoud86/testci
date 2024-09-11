01949ac919a817af4e8946b6f75c513a76673a89
ahmedaboelsoud86 <ahmed_A_h_a@hotmail.com>
2024-09-11 19:46:57 +0300

    run test3

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/test');
 
-        $response->assertStatus(403);
+        $response->assertStatus(200);
     }
 }
