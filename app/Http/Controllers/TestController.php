4c1727e98f325e0dc11fdc2175fd68e18a12ef1a
ahmedaboelsoud86 <ahmed_A_h_a@hotmail.com>
2024-09-11 19:05:30 +0300

    first ci

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,7 @@ class TestController extends Controller
 {
     public function __construct()
     {
-         $this->middleware('permission:patient')->only(['index']);
+        /// $this->middleware('permission:patient')->only(['index']);
         // $this->middleware('permission:create_movies')->only(['create', 'store']);
         // $this->middleware('permission:update_movies')->only(['edit', 'update']);
         // $this->middleware('permission:delete_movies')->only(['delete', 'bulk_delete']);
